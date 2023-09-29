ac6d9867516a49e5dc31b9db7933ff1973134133
Dhvani Thakkar <dhvani.r.thakkar@gmail.com>
2023-09-29 20:44:56 +0530

    Day 15 Project - coffee machine model with given requirements
    1.Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
    a. Check the user’s input to decide what to do next.
    b. The prompt should show every time action has completed, e.g. once the drink is dispensed. The prompt should show again to serve the next customer.
    
    2. Turn off the Coffee Machine by entering “off” to the prompt.
    a. For maintainers of the coffee machine, they can use “off” as the secret word to turn off the machine. Your code should end execution when this happens.
    
    3. Print report.
    a. When the user enters “report” to the prompt, a report should be generated that shows the current resource values. e.g.
    Water: 100ml
    Milk: 50ml
    Coffee: 76g
    Money: $2.5
    
    4. Check resources sufficient?
    a. When the user chooses a drink, the program should check if there are enough resources to make that drink.
    b. E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should not continue to make the drink but print: “Sorry there is not enough water.”
    c. The same should happen if another resource is depleted, e.g. milk or coffee.
    
    5. Process coins.
    a. If there are sufficient resources to make the drink selected, then the program should prompt the user to insert coins.
    b. Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
    c. Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
    pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
    
    6. Check transaction successful?
    a. Check that the user has inserted enough money to purchase the drink they selected.
    E.g Latte cost $2.50, but they only inserted $0.52 then after counting the coins the program should say “Sorry that's not enough money. Money refunded.”.
    b. But if the user has inserted enough money, then the cost of the drink gets added to the machine as the profit and this will be reflected the next time “report” is triggered. E.g.
    Water: 100ml
    Milk: 50ml
    Coffee: 76g
    Money: $2.5
    c. If the user has inserted too much money, the machine should offer change.
    E.g. “Here is $2.45 dollars in change.” The change should be rounded to 2 decimal places.
    
    7. Make Coffee.
    a. If the transaction is successful and there are enough resources to make the drink the user selected, then the ingredients to make the drink should be deducted from the coffee machine resources.
    E.g. report before purchasing latte:
    Water: 300ml
    Milk: 200ml
    Coffee: 100g
    Money: $0
    Report after purchasing latte:
    Water: 100ml
    Milk: 50ml
    Coffee: 76g
    Money: $2.5
    b. Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”. If latte was their choice of drink.

---
+++ Day15_Coffee_Machine.py
@@ -0,0 +1,118 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+money_in_machine = 0.0
+money_inserted = 0.0
+###########################
+
+def insert_money():
+    print("Please insert coins.")
+    quarters = int(input("How many quarters? :"))
+    dimes = int(input("How many dimes? :"))
+    nickels = int(input("How many nickels? :"))
+    pennies = int(input("How many pennies? :"))
+
+    return quarters*0.25 + dimes*0.10 + nickels*0.05 + pennies*0.01
+
+
+def enough_resources(choice):
+    enough_resources = True
+
+    if resources['water'] < MENU[choice]["ingredients"]["water"]:
+        print("Sorry there is not enough water")
+        enough_resources = False
+    
+    if resources['coffee'] < MENU[choice]["ingredients"]["coffee"]:
+        print("Sorry there is not enough coffee")
+        enough_resources = False
+    
+    if 'milk' in MENU[choice]["ingredients"]:
+        if resources['milk'] < MENU[choice]["ingredients"]["milk"]:
+            print("Sorry there is not enough milk")
+            enough_resources = False
+    
+    return enough_resources
+
+
+def coffee_dispensed(choice):
+    resources['water'] -= MENU[choice]["ingredients"]["water"]
+    resources['coffee'] -= MENU[choice]["ingredients"]["coffee"]
+    if 'milk' in MENU[choice]["ingredients"]:
+        resources['milk'] -= MENU[choice]["ingredients"]["milk"]
+    change = money_inserted - MENU[choice]["cost"]
+
+    print(f"Here is ${change:.2f} in change.")
+    print(f"Here is your {choice}. Enjoy!")
+
+
+def coffee_machine(): 
+    global resources
+    global money_in_machine
+    global money_inserted
+
+
+    choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+    while choice not in ["espresso", "latte", "cappuccino", "report", "off", "replenish"]:
+        choice = input("Invalid choice. Enter what you would like: ").lower()
+    
+    if choice == 'off':
+        print("Thank you!")
+        return
+    
+    if choice == 'report':
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${money_in_machine}")
+    
+    if choice == 'replenish':
+        resources['water'] += int(input("Water Added in ml: "))
+        resources['milk'] += int(input("Milk Added in ml: "))
+        resources['coffee'] += int(input("Coffee Added in g: "))
+    
+    else:
+        
+
+        if enough_resources(choice):
+
+            money_inserted = insert_money()
+
+            if money_inserted < MENU[choice]["cost"]:
+                print(f"Sorry, that's not enough money. ${money_inserted} refunded.")
+
+            else:
+                coffee_dispensed(choice)
+                money_in_machine += MENU[choice]["cost"]
+        
+    coffee_machine()
+
+coffee_machine()
